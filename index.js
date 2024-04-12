de1ea472b076d5b96ba842242474dd7fda304548
JMass2001 <156865380+JMass2001@users.noreply.github.com>
2024-04-11 19:11:25 -0500

    Updated (GUI Now Working)

--- index.js
+++ index.js
@@ -7,36 +7,45 @@
 // * Why are you get a warning in your console? Fix it.
 // * Delete these comment lines!
 
-const stone = null
+let stone = null
 
 // this function is called when a row is clicked. 
 // Open your inspector tool to see what is being captured and can be used.
 const selectRow = (row) => {
   const currentRow = row.getAttribute("data-row")
   
-  console.log("Yay, we clicked an item", row)
-  console.log("Here is the stone's id: ", row.id)
-  console.log("Here is the stone's data-size: ", currentRow)
+  //console.log("Yay, we clicked an item", row)
+  //console.log("Here is the stone's id: ", row.id)
+  //console.log("Here is the stone's data-size: ", currentRow)
 
-  pickUpStone(row.id)
+  if(!stone){
+    pickUpStone(row.id)
+  } else {
+    dropStone(row.id)
+  }
 } 
 
 // this function can be called to get the last stone in the stack
 // but there might be something wrong with it...
 const pickUpStone = (rowID) => {
+  console.log("pick up stone function")
   const selectedRow = document.getElementById(rowID);
-  stone = selectedRow.removeChild(selectedRow.lastChild);
-  console.log(stone)
+  console.log("selectedrow:", selectedRow.lastElementChild)
+  stone = selectedRow.removeChild(selectedRow.lastElementChild);
+console.log(stone)
 }
 
 // You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
 // Once you figure that out you'll need to figure out if its a legal move...
 // Something like: if(!stone){pickupStone} else{dropStone}
 
-const dropStone = (rowID, stone) => {
+const dropStone = (rowID) => {
+  console.log("drop stone function")
   document.getElementById(rowID).appendChild(stone)
   stone = null
 }
+
+
 
 // * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
 
